315bc3a4571a21a436eeb3d37993ea18ec454630
Mohammed <tuniis@live.com>
2020-08-13 22:31:06 -0500

    Initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'angular-browser-test';
+  url = 'http://www.google.com';
+
+  whiteListUrls = ['http://www.google.com'];
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+  }
+
+  onButtonClick() {
+    console.log('You have clicked');
+
+    const referrer = document.referrer;
+    console.log('document.referrer: ' + referrer);
+
+    if (this.url && this.whiteListUrls.includes(this.url)) {
+      // window.location.href = '...'
+
+      console.log('About to redirect to ' + this.url);
+      this.document.location.href = this.url;
+    } else {
+      console.log('URL value not allowed.');
+    }
+
+  }
 }
